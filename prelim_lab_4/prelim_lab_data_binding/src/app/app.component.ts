4ab3680d379111e17a0e0d56ef68b12df3b58b67
Rakishi pogi <rakishiduelle@gmail.com>
2024-12-09 13:04:27 +0800

    Angular netlify

--- prelim_lab_4/prelim_lab_data_binding/src/app/app.component.ts
+++ prelim_lab_4/prelim_lab_data_binding/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { DataBindingDemoComponent } from './data-binding-demo/data-binding-demo.component';
+import { DirectivesDemoComponent } from './directives-demo/directives-demo.component';
+import { HomeComponent } from './admin/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DataBindingDemoComponent],
+  imports: [RouterOutlet,DataBindingDemoComponent,DirectivesDemoComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
